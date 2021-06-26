8a4264330814fa5cb25272369f7cc889a712a1f0
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-06-26 16:24:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,16 @@ input.onButtonPressed(Button.A, function () {
         Alt = 0
     }
     Xwing.set(LedSpriteProperty.Y, Alt)
+    Missle.set(LedSpriteProperty.Y, Alt)
 })
 input.onButtonPressed(Button.AB, function () {
-    Missle = game.createSprite(Xwing.get(LedSpriteProperty.X), Xwing.get(LedSpriteProperty.Y))
+    soundExpression.spring.play()
     while (!(4 == Missle.get(LedSpriteProperty.X))) {
         Missle.move(1)
         basic.pause(125)
     }
-    Missle.delete()
+    Missle.set(LedSpriteProperty.X, Xwing.get(LedSpriteProperty.X))
+    Missle.set(LedSpriteProperty.Y, Xwing.get(LedSpriteProperty.Y))
 })
 input.onButtonPressed(Button.B, function () {
     Alt += 1
@@ -19,6 +21,7 @@ input.onButtonPressed(Button.B, function () {
         Alt = 4
     }
     Xwing.set(LedSpriteProperty.Y, Alt)
+    Missle.set(LedSpriteProperty.Y, Alt)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(game.score())
@@ -30,6 +33,7 @@ let Xwing: game.LedSprite = null
 let Alt = 0
 game.setScore(0)
 game.setLife(5)
+music.playMelody("C G F E D C5 G - ", 107)
 images.createBigImage(`
     . . . . . . . . . .
     . . . # . # # # . .
@@ -39,18 +43,18 @@ images.createBigImage(`
     `).scrollImage(1, 200)
 Alt = 2
 Xwing = game.createSprite(0, 2)
+Missle = game.createSprite(Xwing.get(LedSpriteProperty.X), Xwing.get(LedSpriteProperty.Y))
 basic.forever(function () {
     basic.pause(randint(500, 3000))
     Tie1 = game.createSprite(4, randint(0, 4))
     Tie1.turn(Direction.Right, 180)
     for (let index = 0; index <= 4; index++) {
         Tie1.move(1)
-        if (Tie1.isTouching(Missle)) {
-            game.addScore(50)
-            Tie1.delete()
-        }
         if (Tie1.isTouching(Xwing)) {
             game.removeLife(1)
+        }
+        if (Missle.isTouching(Tie1)) {
+            game.addScore(25)
         }
         basic.pause(200)
     }
@@ -59,15 +63,14 @@ basic.forever(function () {
 basic.forever(function () {
     basic.pause(randint(500, 3000))
     Tie2 = game.createSprite(4, randint(0, 4))
-    Tie1.turn(Direction.Right, 180)
+    Tie2.turn(Direction.Right, 180)
     for (let index = 0; index <= 4; index++) {
         Tie2.move(1)
-        if (Tie2.isTouching(Missle)) {
-            game.addScore(50)
-            Tie2.delete()
-        }
         if (Tie2.isTouching(Xwing)) {
             game.removeLife(1)
+        }
+        if (Missle.isTouching(Tie2)) {
+            game.addScore(25)
         }
         basic.pause(200)
     }
